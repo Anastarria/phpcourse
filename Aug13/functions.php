9363e583403a12abbb1713a184764dc787f049c8
Anastasia Zhuravel <zhuravelanastasia444@gmail.com>
2020-08-17 22:34:14 +0300

    CSV form.ver1

--- Aug13/functions.php
+++ Aug13/functions.php
@@ -2,12 +2,54 @@
 
 const USERS_FILE = 'data.json';
 
+function addToTable() {
+  $users = getContactData();
+
+  dataTransform($users);
+}
+function getContactData(): array
+{
+    $jsonString = file_get_contents(USERS_FILE);
+    return json_decode($jsonString, true) ?? [];
+}
+
+function dataTransform() {
+  $path = "";
+  $path = $path . basename( $_FILES['newfile']['name']);
+  if(!empty($_FILES['newfile']))
+  {
+    $fileType = strtolower(pathinfo($_FILES['newfile']['name'],PATHINFO_EXTENSION));
+    if($fileType != "csv") {
+    die ("Sorry, only CSV files are allowed.");
+    $uploadOk = 0;
+}
+      elseif(move_uploaded_file($_FILES['newfile']['tmp_name'], $path)) {
+
+
+
+      $fp = fopen($_FILES['newfile']['name'], 'r');
+      $key = fgetcsv($fp,"1024",",");
+      // parse csv rows into array
+      $json = array();
+        while ($row = fgetcsv($fp,"1024",",")) {
+        $json[] = array_combine($key, $row);
+         }
+      $encode = json_encode($json);
+      file_put_contents(USERS_FILE, $encode);
+
+      fclose($fp);
+      echo "<br>The table has been updated with the data from  ".  basename( $_FILES['newfile']['name']). " file";
+      }
+
+   }
+}
+
 function showTableform()
 {
   $html = '
-  <form action="http://localhost:20777?action=create" method="post">
+  <form action="http://localhost:9000?action=create" method="post" enctype="multipart/form-data">
         <input name="newfile" type="file">
-        <button type="submit" name="submit" class="btn btn-dark">Submit</button>
+        <button type="submit" name="import" class="btn btn-dark">Import Data</button>
   </form>
   ';
   echo $html;
@@ -26,31 +68,10 @@ function showContactTable()
   <tbody>';
 
     foreach (getContactData() as $contact) {
-      $html .= "<tr><td>{$contact['user']}</td><td>{$contact['phone']}</td><td><img src='uploads/images.png' width='150'></td></tr>";
+      $html .= "<tr><td>{$contact['ID']}</td><td>{$contact['Email']}</td><td>{$contact['Phone']}</td></tr>";
     }
 
     $html .= '</tbody></table>';
 
   echo $html;
 }
-//
-// function getContactData(): array
-// {
-//     $jsonString = file_get_contents(USERS_FILE);
-//     return json_decode($jsonString, true) ?? [];
-// }
-//
-// function dataTransform() {  //переделать эту функцию чтобы она считывала данные
-//   $users = getContactData();
-//   $users[] = [
-//     'user' => $name,
-//     'phone' => $phone
-//   ];
-//   writeUsers($users);
-// }
-//
-// function writeUsers(array $users){
-//   $json = json_encode($users);
-//
-//   file_put_contents(USERS_FILE, $json);
-// }
